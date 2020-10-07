6c592de31dca1dd169733c46257ef8ab03f2f4e2
GabrielBR16 <gabrieldiques@gmail.com>
2020-10-07 19:17:04 +0100

    Completed all steps of TDD metholodogy. The first test is return only numbers because the result isn´t Fizz and isn´t Buzz

--- src/test/java/test/FizzBuzz_.java
+++ src/test/java/test/FizzBuzz_.java
@@ -1,5 +1,27 @@
 package test;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzz_ {
 
+    private FizzBuzz fizzBuzz;
+
+    @Before
+    public void setUp() throws Exception {
+        fizzBuzz = new FizzBuzz();
+    }
+
+    @Test
+    public void should_return_number_when_it_is_not_fizz_and_not_buzz() {
+        assertThat(fizzBuzz.of(1)).isEqualTo("1");
+    }
+
+    private class FizzBuzz {
+        public String of(int number) {
+            return String.valueOf(number);
+        }
+    }
 }
